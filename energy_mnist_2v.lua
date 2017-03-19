2335d6cf70bd3f6b593cae8094444d70c486c824
shillel <shillel@campus.technion.ac.il>
2017-03-19 10:37:27 +0200

    Update energy_mnist_2v.lua

--- energy_mnist_2v.lua
+++ energy_mnist_2v.lua
@@ -9,22 +9,11 @@ testLabels = mnist.testdataset().label:add(1);
 
 require 'nn'
 require 'cunn'
-require 'optim'
-require 'gnuplot'
 
 model = torch.load("model_mnist_2v.dat")
 local w, dE_dw = model:getParameters()
 print('Number of parameters:', w:nElement())
 print(tostring(model))
-print('--------- Fischer --------------')
--- print(model:get(6):get(3):get(1):get(1):get(1))
--- print(model:get(6):get(3):get(1):get(1):get(1).weight:topk(3))
--- print(model:get(6):get(3):get(1):get(1):get(1).weight:topk(3,true))
--- print('-----------------------')
--- print(model:get(6):get(3):get(1):get(2):get(1).weight:topk(3))
--- print(model:get(6):get(3):get(1):get(2):get(1).weight:topk(3,true))
--- print('-----------------------')
---print(model:get(6):get(3):get(1):get(1):get(1):getParameters():topk(5))
 print('-----------------------')
 a,b = model:get(6):get(3):get(1):get(1):get(1).weight:sum(1):mul(-10):sort()
 print('vecA:')
@@ -32,20 +21,3 @@ print(a:reshape(128,1))-- , b:reshape(128,1),2)
 a,b = model:get(6):get(3):get(1):get(2):get(1).weight:sum(1):mul(-10):sort()
 print('vecB:')
 print(a:reshape(128,1))
-
-
-print('-----------------------')
---print(model:get(6):get(3):get(1):get(2):get(1).weight:sum(1):mul(-10):sort())
-print('-----------------------')
-
--- print(model:get(6):get(3):get(1):get(2):get(1):getParameters():topk(5))
-print('-----------------------')
-
-
-local range = torch.range(1, 10)
-
--- gnuplot.pngfigure('last_layer.png')
--- gnuplot.plot({'trainLoss',trainLoss},{'testLoss',testLoss})
--- gnuplot.xlabel('epochs')
--- gnuplot.ylabel('Loss')
--- gnuplot.plotflush()
