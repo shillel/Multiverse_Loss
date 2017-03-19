33023ba8b0cc6c9c35206da7b44b34d90e27eae2
shillel <shillel@campus.technion.ac.il>
2017-03-19 10:50:20 +0200

    Update energy_nin_2v.lua

--- nin/energy_nin_2v.lua
+++ nin/energy_nin_2v.lua
@@ -3,58 +3,24 @@
 
 require 'nn'
 require 'cunn'
-require 'optim'
-require 'gnuplot'
-require 'trepl'
+
 model = torch.load("model_2v.dat")
 local w, dE_dw = model:getParameters()
 print('Number of parameters:', w:nElement())
 print(tostring(model))
-print('--------- Fischer --------------')
+
 -- 
 print(model:get(25):get(1):get(1):get(1).weight[1][1])
 print(model:get(25):get(3):get(1):get(1):get(1).weight[1][1])
 
--- print(model:get(6):get(3):get(1):get(1):get(1).weight:topk(3))
--- print(model:get(6):get(3):get(1):get(1):get(1).weight:topk(3,true))
--- print('-----------------------')
--- print(model:get(6):get(3):get(1):get(2):get(1).weight:topk(3))
--- print(model:get(6):get(3):get(1):get(2):get(1).weight:topk(3,true))
--- print('-----------------------')
---print(model:get(6):get(3):get(1):get(1):get(1):getParameters():topk(5))
 
-
---os.exit()
 
 
 print('-----------------------')
---a,b = model:get(6):get(4):get(1):get(1):get(1).weight:sum(1):mul(10):abs():sort()
 a,b = model:get(25):get(3):get(1):get(1):get(1).weight:sum(1):mul(1):sort()
 print('vecA1:')
---print(a:reshape(256,1))-- , b:reshape(128,1),2)
---a,b = model:get(6):get(4):get(1):get(2):get(1).weight:sum(1):mul(10):abs():sort()
+print(a:reshape(256,1))
+
 a,b = model:get(25):get(3):get(1):get(2):get(1).weight:sum(1):mul(1):sort()
 print('vecA2:')
---print(a:reshape(256,1))
-
---a,b = model:get(24).weight:sum(2):mul(1):sort()
---print('vecR:')
---print(a:reshape(256,1))
-
-
-
-print('-----------------------')
---print(model:get(6):get(3):get(1):get(2):get(1).weight:sum(1):mul(-10):sort())
-print('-----------------------')
-
--- print(model:get(6):get(3):get(1):get(2):get(1):getParameters():topk(5))
-print('-----------------------')
-
-
---local range = torch.range(1, 10)
-
--- gnuplot.pngfigure('last_layer.png')
--- gnuplot.plot({'trainLoss',trainLoss},{'testLoss',testLoss})
--- gnuplot.xlabel('epochs')
--- gnuplot.ylabel('Loss')
--- gnuplot.plotflush()
+print(a:reshape(256,1))
