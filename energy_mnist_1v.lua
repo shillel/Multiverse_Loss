ee119b60f85f096ba87cbe2b121db409db122e86
shillel <shillel@campus.technion.ac.il>
2017-03-19 10:36:29 +0200

    Update energy_mnist_1v.lua

--- energy_mnist_1v.lua
+++ energy_mnist_1v.lua
@@ -16,33 +16,9 @@ model = torch.load("model_mnist_1v.dat")
 local w, dE_dw = model:getParameters()
 print('Number of parameters:', w:nElement())
 print(tostring(model))
-print('--------- Fischer --------------')
--- print(model:get(6):get(3):get(1):get(1):get(1))
--- print(model:get(6):get(3):get(1):get(1):get(1).weight:topk(3))
--- print(model:get(6):get(3):get(1):get(1):get(1).weight:topk(3,true))
--- print('-----------------------')
--- print(model:get(6):get(3):get(1):get(2):get(1).weight:topk(3))
--- print(model:get(6):get(3):get(1):get(2):get(1).weight:topk(3,true))
--- print('-----------------------')
---print(model:get(6):get(3):get(1):get(1):get(1):getParameters():topk(5))
+
 print('-----------------------')
 a,b = model:get(6).weight:sum(1):mul(-10):sort()
 print('vecA:')
-print(a:reshape(128,1))-- , b:reshape(128,1),2)
+print(a:reshape(128,1))
 
-
-print('-----------------------')
---print(model:get(6):get(3):get(1):get(2):get(1).weight:sum(1):mul(-10):sort())
-print('-----------------------')
-
--- print(model:get(6):get(3):get(1):get(2):get(1):getParameters():topk(5))
-print('-----------------------')
-
-
-local range = torch.range(1, 10)
-
--- gnuplot.pngfigure('last_layer.png')
--- gnuplot.plot({'trainLoss',trainLoss},{'testLoss',testLoss})
--- gnuplot.xlabel('epochs')
--- gnuplot.ylabel('Loss')
--- gnuplot.plotflush()
